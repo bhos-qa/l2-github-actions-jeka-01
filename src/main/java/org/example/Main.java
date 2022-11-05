41cb5b976bec05ce7dff234c3031692e016a26f4
jeka-01 <zeka.abbasov.bhoa@gmail.com>
2022-11-05 14:50:07 +0400

    lab2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static String stringLength (String word){
+        int length = word.length();
+        return "Length of the word '" + word + "' is " + length;
+    }
+}
